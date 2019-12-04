53320e8345abaa96a9b6e85f9edff4a76003e222
yoav.keissar <yoav.keissar@servicenow.com>
2019-12-04 21:00:07 +0200

    aa

--- localapp/src/main/java/EC2Launch.java
+++ localapp/src/main/java/EC2Launch.java
@@ -5,35 +5,31 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.InstanceType;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class EC2Launch {
     public static void main(String[] args) throws Exception {
-        ProfileCredentialsProvider profile = new ProfileCredentialsProvider();
-        AWSCredentials  cred =profile.getCredentials() ;
 
-        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(cred);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(new ProfileCredentialsProvider().getCredentials());
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
                 .withCredentials(credentialsProvider)
-                .withRegion("us-west-2")
+                .withRegion(Regions.US_WEST_2)
                 .build();
 
-        AWSCredentialsProvider credentialsProvider2 = new AWSStaticCredentialsProvider(new ProfileCredentialsProvider().getCredentials());
         AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
                 .withCredentials(credentialsProvider)
-                .withRegion("us-west-2")
+                .withRegion(Regions.US_WEST_2)
                 .build();
-
         try {
             // Basic 32-bit Amazon Linux AMI 1.0 (AMI Id: ami-08728661)
-            RunInstancesRequest request = new RunInstancesRequest("ami-8c1fece5", 1, 1);
-            request.setInstanceType(InstanceType.T1Micro.toString());
+            RunInstancesRequest request = new RunInstancesRequest("ami-b66ed3de", 1, 1);
+            request.setInstanceType(InstanceType.T2Micro.toString());
             List<Instance> instances = ec2.runInstances(request).getReservation().getInstances();
             System.out.println("Launch instances: " + instances);
 
